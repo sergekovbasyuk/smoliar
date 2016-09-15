67d5a61bbed11fc3fc12a04adeefd7566569dcb2
sergekovbasyuk <kolbasovs@gmail.com>
2016-09-15 20:13:37 +0300

    Update 2016-09-15T17:13:33.859Z

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#fullpage').fullpage({
-      menu: '#menu',
-      anchors:['first', 'about', 'works', 'contact'],
-      css3: true,
-      navigation: true,
-      navigationPosition: 'right'
-    });
-});
